401e2d315bc2a5be89b34b5fdcfce2f8dc38dc84
Raúl Moreno <raulmoreno899@gmail.com>
2019-09-25 14:40:54 -0500

    Project created

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const MyCalendar = props => (
+  <div>
+    
+  </div>
+)
